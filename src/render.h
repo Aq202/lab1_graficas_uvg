7da7c87b066d40666856301ea7682b21e1a74cd8
Diego <diegoguatedb2002@gmail.com>
2023-07-14 22:41:07 -0600

    Figura 2 terminada

--- src/render.h
+++ src/render.h
@@ -3,5 +3,23 @@
 
 void render()
 {
-  
+
+  setClearColor({0, 0, 0});
+  clear();
+
+  std::vector<Vertex2> points;
+  points.push_back(Vertex2{321, 335});
+  points.push_back(Vertex2{288, 286});
+  points.push_back(Vertex2{339, 251});
+  points.push_back(Vertex2{374, 302});
+
+  setCurrentColor({35, 29, 226});
+
+  fillPolygon(points);
+
+  setCurrentColor({255, 255, 255});
+
+  drawPolygon(points);
+
+  renderBuffer();
 }
