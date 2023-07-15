51a37895db7a329203515731ec516b3c0e84c6e0
Diego <diegoguatedb2002@gmail.com>
2023-07-14 22:45:15 -0600

    Poligono 3 terminado

--- src/render.h
+++ src/render.h
@@ -3,5 +3,22 @@
 
 void render()
 {
-  
+  setClearColor({0, 0, 0});
+  clear();
+
+  // Dibujar una línea diagonal
+  std::vector<Vertex2> points;
+  points.push_back(Vertex2{377, 249});
+  points.push_back(Vertex2{411, 197});
+  points.push_back(Vertex2{436, 249});
+
+  setCurrentColor({255, 0, 0});
+
+  fillPolygon(points);
+
+  setCurrentColor({255, 255, 255});
+
+  drawPolygon(points);
+
+  renderBuffer();
 }
